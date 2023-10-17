9549fa622818e02f9ae66cf737ef3fe8ecc835fd
Attirobert <karobatti@gmail.com>
2023-10-17 16:20:15 +0200

    iktat.sql hozzá adva

---
+++ Iktatas/Data/dbo.iktat.data.sql
@@ -0,0 +1,8 @@
+SET IDENTITY_INSERT [dbo].[iktat] ON
+INSERT INTO [dbo].[iktat] ([Id_iktat], [Iktatoszam], [IdUser], [Datum], [IdTema], [Leiras]) VALUES (2, N'458                 ', 3, N'2023-10-25', 2, N'uvtft                                             ')
+INSERT INTO [dbo].[iktat] ([Id_iktat], [Iktatoszam], [IdUser], [Datum], [IdTema], [Leiras]) VALUES (3, N'2023/014            ', 3, N'2023-10-10', 2, N'iofrdzkj                                          ')
+INSERT INTO [dbo].[iktat] ([Id_iktat], [Iktatoszam], [IdUser], [Datum], [IdTema], [Leiras]) VALUES (4, N'2023/015            ', 5, N'2023-10-11', 1, N'iőhourcz u9z8 v7                                  ')
+INSERT INTO [dbo].[iktat] ([Id_iktat], [Iktatoszam], [IdUser], [Datum], [IdTema], [Leiras]) VALUES (5, N'2023/016            ', 6, N'2023-10-11', 1, N'nukguf uhuzz                                      ')
+INSERT INTO [dbo].[iktat] ([Id_iktat], [Iktatoszam], [IdUser], [Datum], [IdTema], [Leiras]) VALUES (6, N'2023/017            ', 5, N'2023-10-11', 1, N'hkhui                                             ')
+INSERT INTO [dbo].[iktat] ([Id_iktat], [Iktatoszam], [IdUser], [Datum], [IdTema], [Leiras]) VALUES (7, N'20231010/1          ', 6, N'2023-10-10', 4, N'iubvzd ftdfr                                      ')
+SET IDENTITY_INSERT [dbo].[iktat] OFF
---
+++ Iktatas/Data/dbo.iktat.sql.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[iktat] (
+    [Id_iktat]   INT        IDENTITY (1, 1) NOT NULL,
+    [Iktatoszam] NCHAR (20) NOT NULL,
+    [IdUser]     INT        NOT NULL,
+    [Datum]      DATE       NOT NULL,
+    [IdTema]     INT        NOT NULL,
+    [Leiras]     NCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id_iktat] ASC),
+    CONSTRAINT [FK_iktat_Temak] FOREIGN KEY ([IdTema]) REFERENCES [dbo].[Temak] ([Id_Tema]),
+    CONSTRAINT [FK_iktat_Users] FOREIGN KEY ([IdUser]) REFERENCES [dbo].[Users] ([Id_user])
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IX_iktat_Iktatoszam]
+    ON [dbo].[iktat]([Iktatoszam] ASC);
+
